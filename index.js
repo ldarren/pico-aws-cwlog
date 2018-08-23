8473beadffe2666b00fc41d3acf81878bd7e9643
Darren Liew <ldarren@gmail.com>
2018-08-23 13:01:54 +0000

    start time based on stream creation

--- index.js
+++ index.js
@@ -3,26 +3,24 @@ const AWS = require('aws-sdk')
 const pico = require('pico-common')
 const pObj = pico.export('pico/obj')
 
+const debug = process.env.DEBUG ? function(){ console.log.apply(console, arguments) } : function(){}
+
 const PUBLIC_CONFIG = {
-  cloudwatch: {
-			apiVersion: '2014-03-28'
-  },
-  streams: {},
-  uploadInterval: 60000,
-  uploadBatchSize: 50
+	cloudwatch: {
+		apiVersion: '2014-03-28'
+	},
+	streams: [],
+	uploadInterval: 60000,
+	uploadBatchSize: 50
 }
 
 const LOGGERS = {}
 const CONTEXTS = []
 
-function debug(){
-	if (!process.env.DEBUG) console.log.apply(console, arguments)
-}
-
 function cleanup(err) {
     if (err) debug(err)
     debug('clean up on exit')
-		CONTEXTS.forEach(upload)
+	CONTEXTS.forEach(upload)
 }
 
 function exit(){
@@ -32,7 +30,8 @@ function exit(){
 
 function exception(exp){
 	cleanup()
-  throw exp
+	console.error(exp)
+	throw exp
 }
 
 // do something when app is closing
@@ -47,99 +46,101 @@ process.on('uncaughtException', exception)
 
 function upload(ctx){
 	clearTimeout(ctx.timeoutId)
-  if (!Object.keys(ctx.tokens).length) {
-    // try again in 1 sec
-	  return ctx.timeoutId = setTimeout(upload, 1000, ctx)
-  }
-	ctx.timeoutId = setTimeout(upload, ctx.cfg.uploadInterval, ctx)
 
 	const log = ctx.log
 	ctx.log = {}
-
 	Object.keys(log).forEach(sname => {
-    ctx.cw.putLogEvents({
-      logEvents: log[sname],
-      logGroupName: ctx.groupName,
-      logStreamName: sname,
-      sequenceToken: ctx.tokens[sname] || undefined
-    }, (err, res) => {
-      if (err) return debug(err)
-      ctx.tokens[sname] = res.nextSequenceToken
-    })
-  })
+		ctx.cw.putLogEvents({
+			logEvents: log[sname],
+			logGroupName: ctx.groupName,
+			logStreamName: sname,
+			sequenceToken: ctx.tokens[sname] || undefined
+		}, (err, res) => {
+			if (err) return debug(err)
+			ctx.tokens[sname] = res.nextSequenceToken
+		})
+	})
+
+	ctx.timeoutId = setTimeout(upload, ctx.cfg.uploadInterval, ctx)
 }
 
 const Handler = {
 	get(ctx, propKey, receiver) {
 		return function(){
 			const arr = ctx.log[propKey] = ctx.log[propKey] || []
-			arr.push(1 < arguments.length ? Array.prototype.slice.call(arguments) : arguments[0])
-			if (ctx.cfg.uploadBatchSize <= Object.keys(ctx.log).reduce((size, key) => (size + ctx.log[key].length), 0))
-				upload(ctx)
+			const msg = 1 < arguments.length ? Array.prototype.slice.call(arguments) : arguments[0]
+			arr.push({message: msg.charAt ? msg : JSON.stringify(msg), timestamp: Date.now()})
+
+			if (checkCond(ctx)) upload(ctx)
 			return 0
 		}
 	}
 }
 
-function Logger(ctx){
-  ctx.cfg.streams.forEach(sname => {
-    this[sname] = function() {
-			const arr = ctx.log[sname] = ctx.log[sname] || []
-      const msg = 1 < arguments.length ? Array.prototype.slice.call(arguments) : arguments[0]
-			arr.push({message: msg.charAt ? msg : JSON.stringify(msg), timestamp: Date.now()})
+function checkCond(ctx){
+	return (ctx.started &&
+		(ctx.cfg.uploadBatchSize <= Object.keys(ctx.log).reduce((size, key) => (size + ctx.log[key].length), 0)))
+}
 
-			if (ctx.cfg.uploadBatchSize <= Object.keys(ctx.log).reduce((size, key) => (size + ctx.log[key].length), 0))
-				upload(ctx)
-			return 0
-    }
-  })
+function start(ctx){
+	clearTimeout(ctx.timeoutId)
+	ctx.started = 1
+	if (checkCond(ctx)) upload(ctx)
+	else ctx.timeoutId = setTimeout(upload, ctx.cfg.uploadInterval, ctx)
+}
+
+function createStreams(ctx, cfg, groupName, createdStreams){
+	createdStreams.reduce((acc, stream) => {
+		acc[stream.logStreamName] = stream.uploadSequenceToken || ''
+		return acc
+	}, ctx.tokens)
+
+	const snames = Object.keys(ctx.tokens)
+
+	const diff = cfg.streams.filter(sname => -1 === snames.indexOf(sname))
+	let difflen = diff.length
+	cfg.streams = snames
+	if (!difflen) return start(ctx)
+
+	diff.forEach(sname => ctx.cw.createLogStream({ logGroupName: groupName, logStreamName: sname }, () => {
+		cfg.streams.push(sname)
+		if (0 === --difflen) start(ctx)	
+	}))
 }
 
 module.exports = function(gname, config){
 	if (LOGGERS[gname]) return LOGGERS[gname]
 
-  const cfg = pObj.extends({}, [ PUBLIC_CONFIG, config ])
+	const cfg = pObj.extends({}, [ PUBLIC_CONFIG, config ])
 
-  const tokens = {}
+	const tokens = {}
 
-  const ctx = {
-    groupName: gname,
-    cw: new AWS.CloudWatchLogs(cfg.cloudwatch),
-    cfg,
-    tokens,
-    timeoutID: 0,
-    log: {}
-  }
+	const ctx = {
+		groupName: gname,
+		cw: new AWS.CloudWatchLogs(cfg.cloudwatch),
+		cfg,
+		tokens,
+		timeoutID: 0,
+		log: {}
+	}
 
-  CONTEXTS.push(ctx)
-  LOGGERS[gname] = cfg.streams.length ? new Logger(ctx) : new Proxy(ctx, Handler)
+	CONTEXTS.push(ctx)
+	LOGGERS[gname] = new Proxy(ctx, Handler)
 
-  ctx.cw.describeLogStreams({ logGroupName: gname }, (err, res) => {
-    if (err) {
-      switch(err.code){
-      case 'ResourceNotFoundException':
-        return ctx.cw.createLogGroup({ logGroupName: gname }, (err, res) => {
-          if (err && 'ResourceAlreadyExistsException' !== err.code) return cb(err)
-          if (!cfg.streams.length) return
+	ctx.cw.describeLogStreams({ logGroupName: gname }, (err, res) => {
+		if (err) {
+			switch(err.code){
+			case 'ResourceNotFoundException':
+				return ctx.cw.createLogGroup({ logGroupName: gname }, (err, res) => {
+					if (err && 'ResourceAlreadyExistsException' !== err.code) return cb(err)
+					createStreams(ctx, cfg, gname, [])
+				})
+			default:
+				return debug(err)
+			}
+		}
+		createStreams(ctx, cfg, gname, res.logStreams)
+	})
 
-          cfg.streams.forEach(sname => ctx.cw.createLogStream({ logGroupName: gname, logStreamName: sname }, () => {}))
-        })
-      default:
-        return debug(err)
-      }
-    }
-    const snames = res.logStreams.map(stream => {
-      tokens[stream.logStreamName] = stream.uploadSequenceToken || ''
-      return stream.logStreamName
-    })
-    if (cfg.streams.length) {
-      const diff = cfg.streams.filter(sname => -1 === snames.indexOf(sname))
-      if (diff.length) diff.forEach(sname => ctx.cw.createLogStream({ logGroupName: gname, logStreamName: sname }, () => {}))
-    }
-    cfg.streams = snames
-  })
-
-  ctx.timeoutId = setTimeout(upload, ctx.cfg.uploadInterval, ctx)
-
-  return LOGGERS[gname]
+	return LOGGERS[gname]
 }
--- test.js
+++ test.js
@@ -4,39 +4,39 @@ const AWS = require('aws-sdk')
 const pLog = require('./index')
 
 AWS.config.update({
-  accessKeyId: process.env.AWS_ID,
-  secretAccessKey: process.env.AWS_KEY,
-  region: process.env.AWS || 'ap-southeast-1',
-  timeout: 5000
+	accessKeyId: process.env.AWS_ACCESS_ID,
+	secretAccessKey: process.env.AWS_ACCESS_KEY,
+	region: process.env.AWS_DEFAULT_REGION || 'ap-southeast-1',
+	timeout: 5000
 })
 
-pLog('exit_test', {
-  uploadInterval: 60000, // default: 5000
-  uploadBatchSize: 10000, // default: 500
-  streams: ['error', 'log']
+pLog('test.exit', {
+	uploadInterval: 60000, // default: 5000
+	uploadBatchSize: 10000, // default: 500
+	streams: ['error', 'log']
 })
-pLog('batch_test', {
-  uploadInterval: 60000, // default: 5000
-  uploadBatchSize: 1, // default: 500
-  streams: ['error', 'log']
+pLog('test.batch', {
+	uploadInterval: 60000, // default: 5000
+	uploadBatchSize: 1, // default: 500
+	streams: ['error', 'log']
 })
-pLog('timer_test', {
-  uploadInterval: 100, // default: 5000
-  uploadBatchSize: 10000, // default: 500
-  streams: ['error', 'log']
+pLog('test.timer', {
+	uploadInterval: 100, // default: 5000
+	uploadBatchSize: 10000, // default: 500
+	streams: ['error', 'log']
 })
 
-let logger = pLog('exit_test')
+let logger = pLog('test.exit')
 let result = logger.error({code: 1001, msg: 'exit test error'})
 result = result + logger.log('exit test log')
 
-logger = pLog('batch_test')
+logger = pLog('test.batch')
 result = result + logger.error({code: 2001, msg: 'batch test error'})
 result = result + logger.log('batch test log')
 
-logger = pLog('timer_test')
+logger = pLog('test.timer')
 result = result + logger.error({code: 3001, msg: 'timer test error'})
 result = result + logger.log('timer test log')
 setTimeout(() => {
-  process.exit(result)
-}, 2000)
+	process.exit(result)
+}, 30000)
